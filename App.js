b4757422585bf92259b2a405cb7092eeb0e5033b
tchiinhemba <tchiinhemba@gmail.com>
2023-02-03 19:18:30 +0100

    update

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <View style={styles.headerContainer}>
+          <View><Text style={styles.textStyle}>Carteira</Text></View>
+          <View>
+            <TouchableOpacity style={styles.buttonAdd}>
+              <Text>Adicionar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'black',
   },
+  headerContainer: {
+    color: "#fff",
+    paddingTop: 50,
+    paddingRight: 20,
+    paddingLeft: 20,
+    flex: "flex",
+    flexDirection: "column"
+  },
+  textStyle: {
+    color: "#fff",
+    fontSize: 26,
+    fontWeight: "900"
+  },
+  buttonAdd: {
+    backgroundColor: "red"
+  }
 });
